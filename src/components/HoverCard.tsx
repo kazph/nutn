69d49bab83b9b42b9f93c37f3088ec8c87b75e33
Kasper Hoel <kasper-hoel@hotmail.com>
2023-07-11 14:59:27 +0200

    Imporved dark-mode-suppert

--- src/components/HoverCard.tsx
+++ src/components/HoverCard.tsx
@@ -10,25 +10,25 @@ export function Test(props: {
 	return (
 		<HoverCard.Root>
 			<HoverCard.Trigger
-				class="border-2 border-gray-200 hover:border-gray-400 hover:bg-gray-50 rounded-lg p-6 flex flex-col items-center text-center"
+				class="border-2 border-gray-200 hover:border-gray-400 hover:bg-gray-50 rounded-lg p-6 flex flex-col items-center text-center dark:bg-gray-800 dark:border-gray-800 dark:hover:bg-gray-800"
 				href={props.href}
 			>
 				<div
-					class="w-2/5 min-w-[3rem] p-2 fill-gray-700"
+					class="w-2/5 min-w-[3rem] p-2 fill-gray-700 dark:fill-gray-200"
 					innerHTML={props.pictogram}
 				/>
-				<h2 class="font-semibold text-gray-700 text-lg" innerHTML={props.title} />
+				<h2 class="font-semibold text-gray-700 dark:text-gray-200 text-lg" innerHTML={props.title} />
 			</HoverCard.Trigger>
 			<HoverCard.Portal>
-				<HoverCard.Content class="bg-white p-4 w-64 border-2 border-gray-200 rounded shadow-md">
+				<HoverCard.Content class="bg-white dark:bg-gray-800 p-4 w-64 border-2 border-gray-200 rounded shadow-md">
 					<HoverCard.Arrow class="rounded-none" />
-					<h4 class="text-sm font-bold" innerHTML={props.title} />
-					<p>{props.description}</p>
+					<h4 class="text-sm font-bold dark:text-gray-200" innerHTML={props.title} />
+					<p class="dark:text-gray-200">{props.description}</p>
 
 					{props.documentation_page && 
 						<a
 						href={props.documentation_page}
-						class="text-sm text-blue-800 font-semibold hover:underline"
+						class="text-sm text-blue-800 dark:text-blue-500 font-semibold hover:underline"
 						target="_blank"
 						>
 							NTNUs Dokumentasjon
