7ed08e79424719afdbacc5703b489005b2a29125
Kasper Hoel <kasper-hoel@hotmail.com>
2023-08-28 09:22:12 +0200

    Fixed Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
 import { ssr } from 'solid-js/web';
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
+  integrations: [tailwind(), solidJs(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })],
   site: 'https://kazph.github.io',
   base: '/nutn',
   vite: {
